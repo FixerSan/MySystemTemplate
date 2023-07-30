45239c6c5d3ebf54a6a14bea32ed1b5d696397b9
GangSan <ggomarain1302@gmail.com>
2023-07-30 09:48:19 +0900

    수정: 풀 인터페이스 삭제

--- SystemTemplate/Assets/1.Scripts/IPoolObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PoolObject : MonoBehaviour
-{
-    public abstract void SetUp();
-    public abstract void Clear();
-}
